ca989619c1e2e92c9504f233694b9ef9063ef483
Markus Günther <markus.guenther@gmail.com>
2021-12-01 08:31:41 +0100

    Adds type hints to puzzle solution #1

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
 from itertools import pairwise
-from typing import List
+from typing import Callable, List
 
 
 measurements = [int(line.strip()) for line in open('1.in', 'r').readlines()]
@@ -8,11 +8,11 @@ measurements = [int(line.strip()) for line in open('1.in', 'r').readlines()]
 #times_increased = len(list(filter(lambda x: x > 0, [y - x for (x,y) in pairwise(measurements)])))
 #print(times_increased)
 
-def windowed(seq, size):
+def windowed(seq: List[int], size: int) -> int:
     for i in range(len(seq) - size + 1):
         yield(seq[i: i + size])
 
-def times_increased(seq, pairsize, fn):
+def times_increased(seq: List[int], pairsize: int, fn: Callable[[List[int], List[int]], int]) -> int:
     return len(list(filter(lambda x: x > 0, [fn(x, y) for (x, y) in pairwise(windowed(seq, pairsize))])))
 
 print(times_increased(measurements, 1, lambda x,y: sum(y)-sum(x)))
