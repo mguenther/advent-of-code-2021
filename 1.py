939559f927621cc6c04c6455896f2f19d0155346
Markus Günther <markus.guenther@gmail.com>
2021-12-01 08:27:47 +0100

    Refactor solution to puzzle #1

--- 1.py
+++ 1.py
@@ -8,12 +8,12 @@ measurements = [int(line.strip()) for line in open('1.in', 'r').readlines()]
 #times_increased = len(list(filter(lambda x: x > 0, [y - x for (x,y) in pairwise(measurements)])))
 #print(times_increased)
 
-def windowed(sequence, size):
-    for i in range(len(sequence) - size + 1):
-        yield(sequence[i: i + size])
+def windowed(seq, size):
+    for i in range(len(seq) - size + 1):
+        yield(seq[i: i + size])
 
 def times_increased(seq, pairsize, fn):
-    return len(list(filter(lambda x: x > 0, [fn(x, y) for (x, y) in pairwise(windowed(measurements, pairsize))])))
+    return len(list(filter(lambda x: x > 0, [fn(x, y) for (x, y) in pairwise(windowed(seq, pairsize))])))
 
 print(times_increased(measurements, 1, lambda x,y: sum(y)-sum(x)))
 print(times_increased(measurements, 3, lambda x,y: sum(y)-sum(x)))
