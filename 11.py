8973d1e90817a4bf722cac71feabbb532fe17417
Markus Günther <markus.guenther@gmail.com>
2021-12-12 12:40:12 +0100

    Refactors solution for day 11

--- 11.py
+++ 11.py
@@ -1,17 +1,24 @@
-def read():
-    return [[int(n) for n in line.strip()] for line in open('11.in', 'r')]
+from typing import List
 
 
-def flash(row, col, G):
+def read(filename = '11.in') -> List[List[int]]:
+    return [[int(n) for n in line.strip()] for line in open(filename, 'r')]
+
+
+def flash(row: int, col: int, G: List[List[int]]) -> int:
+    def out_of_bounds(row, col):
+        return row < 0 or col < 0 or row >= len(G) or col >= len(G[row])
+    def flashed_already(row, col):
+        return G[row][col] == -1
     flashes = 1
     G[row][col] = -1
     for dr in [-1, 0, 1]:
         for dc in [-1, 0, 1]:
             row_n = row + dr
             col_n = col + dc
-            if row_n < 0 or col_n < 0 or row_n >= len(G) or col_n >= len(G[row_n]):
+            if out_of_bounds(row_n, col_n):
                 continue
-            if G[row_n][col_n] == -1:
+            if flashed_already(row_n, col_n):
                 continue
             G[row_n][col_n] += 1
             if G[row_n][col_n] >= 10:
@@ -19,7 +26,7 @@ def flash(row, col, G):
     return flashes
 
 
-def total_flashes_up_to(steps = 100):
+def total_flashes_up_to(steps: int = 100) -> int:
     G = read()
     total_flashes = 0
     for step in range(1, steps + 1):
@@ -37,7 +44,7 @@ def total_flashes_up_to(steps = 100):
     return total_flashes
 
 
-def first_synchronized_flash_at(max_steps = 1000):
+def first_synchronized_flash_at(max_steps: int = 1000) -> int:
     G = read()
     for step in range(1, max_steps + 1):
         for row in range(len(G)):
