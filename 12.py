a368c41dcf0910e2d97125dd34013f9709e7fd20
Markus Günther <markus.guenther@gmail.com>
2021-12-12 14:54:44 +0100

    Adds solution to day 12

---
+++ 12.py
@@ -0,0 +1,55 @@
+from collections import defaultdict, deque
+
+
+def to_adjacency_list(filename = '12.in'):
+    E = defaultdict(list)
+    connections = [connection.strip().split('-') for connection in open('12.in', 'r')]
+    for line in connections:
+        l, r = line
+        E[l].append(r)
+        E[r].append(l)
+    return E
+
+
+def is_small_cave(node):
+    return node.lower() == node
+
+
+def number_of_possible_paths():
+    E = to_adjacency_list()
+    Q = deque([('start', set(['start']))])
+    possible_paths = 0
+    while Q:
+        node, visited_small_caves = Q.popleft()
+        if node == 'end':
+            possible_paths += 1
+        for adjacent_node in E[node]:
+            if not adjacent_node in visited_small_caves:
+                extended_visited_small_caves = set(visited_small_caves)
+                if is_small_cave(adjacent_node):
+                    extended_visited_small_caves.add(adjacent_node)
+                Q.append((adjacent_node, extended_visited_small_caves))
+    return possible_paths
+
+
+def number_of_possible_paths_relaxed():
+    E = to_adjacency_list()
+    Q = deque([('start', set(['start']), None)])
+    possible_paths = 0
+    while Q:
+        node, visited_small_caves, visited_twice = Q.popleft()
+        if node == 'end':
+            possible_paths += 1
+        for adjacent_node in E[node]:
+            if not adjacent_node in visited_small_caves:
+                extended_visited_small_caves = set(visited_small_caves)
+                if is_small_cave(adjacent_node):
+                    extended_visited_small_caves.add(adjacent_node)
+                Q.append((adjacent_node, extended_visited_small_caves, visited_twice))
+            elif adjacent_node in visited_small_caves and not visited_twice and adjacent_node not in ['start', 'end']:
+                Q.append((adjacent_node, visited_small_caves, adjacent_node))
+    return possible_paths
+
+
+print(number_of_possible_paths())
+print(number_of_possible_paths_relaxed())
