b129b68b2050f3b804010bded518c9d571f0010a
Markus Günther <markus.guenther@gmail.com>
2021-12-24 21:20:32 +0100

    Adds solution for day 24

---
+++ 24.py
@@ -0,0 +1,8 @@
+# Solved manually after decompiling the input and analyzing it.
+# There is already a great explanation out there, so I won't bother
+# writing another one. Just have a look at the following well-
+# documented code:
+# https://github.com/dphilipson/advent-of-code-2021/blob/master/src/days/day24.rs
+
+print(91699394894995)
+print(51147191161261)
